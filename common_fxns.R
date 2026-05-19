2d1e16f234575cc534d0f8d5603d65ef377253ae
laurelabowd <abowd@bren.ucsb.edu>
2026-05-19 19:53:17 +0000

    Update common_fxns.R

--- common_fxns.R
+++ common_fxns.R
@@ -40,3 +40,65 @@ extract_eez_zonal <- function(id, df, eez_raster) {
     filter(!if_all(c(n_current, n_RCP45_2050, n_RCP45_2100,
                      n_RCP85_2050, n_RCP85_2100), is.na))
 }
+
+# Get entry pixels for a species by EEZ
+extract_eez_entry_pixels_df <- function(id, df, s, eez_spp_entry, eez_raster_df) {
+  spp_filter <- df %>% filter(aphiaid == id)
+  
+  if (nrow(spp_filter) == 0) {
+    warning("No rows found for aphiaid: ", id)
+    return(NULL)
+  }
+  
+  spp <- read_parquet(spp_filter$f) %>%
+    mutate(aphiaid = id) %>% 
+    dplyr::select(x, y, Current, all_of(s), cutoff)
+  
+  # Filter the df of entry into for this species & scenario
+  entry_spp_df <- eez_spp_entry %>% 
+    filter(aphiaid == id) %>% 
+    filter(scenario == s)
+  
+  # Get the EEZs for which the species & scenario is entry
+  spp_entry_eezs <- unique(entry_spp_df$GEONAME)
+  
+  # Join the eez dataframe and filter for the spp_entry_eezs
+  spp_eez <- inner_join(spp, eez_raster_df) %>% 
+    filter(GEONAME %in% spp_entry_eezs) %>% 
+    filter(.data[[s]] >= cutoff) %>% 
+    mutate(binary = 1) %>% 
+    bind_rows()
+  
+  return(spp_eez)
+}
+
+# Get exit pixels for a species by EEZ
+extract_eez_exit_pixels_df <- function(id, df, s, eez_spp_exit, eez_raster_df) {
+  spp_filter <- df %>% filter(aphiaid == id)
+  
+  if (nrow(spp_filter) == 0) {
+    warning("No rows found for aphiaid: ", id)
+    return(NULL)
+  }
+  
+  spp <- read_parquet(spp_filter$f) %>%
+    mutate(aphiaid = id) %>% 
+    dplyr::select(x, y, Current, all_of(s), cutoff)
+  
+  # Filter the df of entry into for this species & scenario
+  exit_spp_df <- eez_spp_exit %>% 
+    filter(aphiaid == id) %>% 
+    filter(scenario == s)
+  
+  # Get the EEZs for which the species & scenario is entry
+  spp_exit_eezs <- unique(exit_spp_df$GEONAME)
+  
+  # Join the eez dataframe and filter for the spp_entry_eezs
+  spp_eez <- inner_join(spp, eez_raster_df) %>% 
+    filter(GEONAME %in% spp_exit_eezs) %>% 
+    filter(Current >= cutoff) %>% 
+    mutate(binary_loss = 1) %>% 
+    bind_rows()
+  
+  return(spp_eez)
+}
